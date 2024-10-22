dd20ebae16a7f0da47cc64cf223bb7df6d727d0f
= <=>
2024-10-22 09:05:27 +0700

    create aplikasi segitiga

---
+++ segitiga.py
@@ -0,0 +1,13 @@
+print("="*40)
+print("PROGRAM SEGITIGA")
+print("="*40)
+
+tinggi = float(input("masukan tinggi: "))
+alas = float(input("masukan alas: "))
+sisi = float(input("masukan sisi: "))
+
+alas = 1/2 * alas * tinggi
+alas = sisi*3
+
+print('luas',luas,'cm2')
+print('alas',alas,'cm2')
