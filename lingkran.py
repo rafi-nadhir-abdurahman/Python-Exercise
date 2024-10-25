38fc7a24b81c5a1bc15f8bf7f5a25bdcd9e0bc99
= <=>
2024-10-25 07:27:04 +0700

    aplikasi lingkaran

---
+++ lingkran.py
@@ -0,0 +1,11 @@
+print("="*40)
+print("PROGRAM LINGKARAN")
+print("="*40)
+
+r = float(input("masukan jari jari: "))
+
+luas = 3*14 * r * r
+keliling = 2*3.14
+
+print('luas',luas,'cm2')
+print('keliling',keliling,'cm2')
