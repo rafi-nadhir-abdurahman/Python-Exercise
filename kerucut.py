766730b64b857b8e0ab5e4df89b1b8ea5d5b3d24
= <=>
2024-11-08 07:34:53 +0700

    krucut

---
+++ kerucut.py
@@ -0,0 +1,10 @@
+print('='*40)
+print('PROGRAM KERUCUT')
+print('='*40)
+
+PHI = 3.14
+r = int(input('masukan jari jari: '))
+s = int(input('masukan sisi: '))
+
+l = PHI * r *(s+r)
+print ("luasnya",l)
