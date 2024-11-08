48dae7e447fce1a91965a6797ea2c01ad92b76e9
= <=>
2024-11-08 07:32:59 +0700

    trapesium

---
+++ trapesium.py
@@ -0,0 +1,11 @@
+print('='*40)
+print('PRORAM TRAPESIUM')
+print('='*40)
+
+PHI = 3.14
+t = int(input('masukan tinggi: '))
+a = int(input('masukan sisi a: '))
+b = int(input('masukan sisi b: '))
+
+L = 1/2 * (a+b) * t
+print('LUAS',L)
